1a08d8da7f1355c1f00746f4a9620b58007bf4ea
Alankrit Vyas <66866845+alankritvyas21@users.noreply.github.com>
2023-10-13 13:13:57 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -5,6 +5,6 @@ $conn = mysqli_connect("localhost", "root", "", "basicbanking");
 
 if (!$conn) {
   die("Could not connect to the database due to the following error --> " . mysqli_connect_error());
-}
+} 
 
 ?>
